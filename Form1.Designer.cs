7e9f69eba84228232b07f184e13a41d747610dd9
Адми <@DESKTOP-FJVRIJS>
2024-12-06 00:46:37 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace To_do_list
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnComplete = new System.Windows.Forms.Button();
+            this.lstTasks = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTask
+            // 
+            this.txtTask.Location = new System.Drawing.Point(12, 39);
+            this.txtTask.Multiline = true;
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(293, 41);
+            this.txtTask.TabIndex = 0;
+            this.txtTask.TextChanged += new System.EventHandler(this.txtTask_TextChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(325, 28);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(96, 52);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(325, 124);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(96, 52);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnComplete
+            // 
+            this.btnComplete.Location = new System.Drawing.Point(325, 284);
+            this.btnComplete.Name = "btnComplete";
+            this.btnComplete.Size = new System.Drawing.Size(96, 52);
+            this.btnComplete.TabIndex = 3;
+            this.btnComplete.Text = "Выполнено";
+            this.btnComplete.UseVisualStyleBackColor = true;
+            this.btnComplete.Click += new System.EventHandler(this.btnComplete_Click);
+            // 
+            // lstTasks
+            // 
+            this.lstTasks.FormattingEnabled = true;
+            this.lstTasks.Location = new System.Drawing.Point(12, 124);
+            this.lstTasks.Name = "lstTasks";
+            this.lstTasks.Size = new System.Drawing.Size(293, 212);
+            this.lstTasks.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите задачу";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Список задач";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 363);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstTasks);
+            this.Controls.Add(this.btnComplete);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtTask);
+            this.Name = "Form1";
+            this.Text = "ToDooooo list";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnComplete;
+        private System.Windows.Forms.ListBox lstTasks;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace To_do_list
+{
+    public partial class Form1 : Form
+    {
+        private const string FilePath = "tasks.txt";
+        private List<string> tasks = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTasks();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string task = txtTask.Text.Trim();
+            if (!string.IsNullOrEmpty(task))
+            {
+                DialogResult result = MessageBox.Show(
+                    "Вы уверены что хотите добавить?",
+                    "Удаление",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    tasks.Add(task);
+                    UpdateTaskList();
+                    txtTask.Clear();
+                    SaveTasks();
+                }
+                else
+                {
+                    return;
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Для начала выберите задачу");
+
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedItem != null)
+            {
+                DialogResult result = MessageBox.Show(
+                  "Вы уверены что хотите удалить?",
+                  "удаление",
+                  MessageBoxButtons.YesNo,
+                  MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    tasks.Remove(lstTasks.SelectedItem.ToString());
+                    UpdateTaskList();
+                    SaveTasks();
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Для начала выберите задачу");
+
+            }
+        }
+
+        private void btnComplete_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedItem != null)
+            {
+                string select = lstTasks.SelectedItem.ToString();
+
+                
+                    if (select.Contains("(Выполнено)"))
+                    {
+                        MessageBox.Show("Задача уже выполнена!");
+                        return;
+                    }
+                    else
+                    {
+                        int index = lstTasks.SelectedIndex;
+                        string completedTask = tasks[index];
+                        tasks[index] = completedTask + " (Выполнено)";
+                        UpdateTaskList();
+                        SaveTasks();
+                    }
+                
+            }
+            else
+            {
+                MessageBox.Show("Выберите задачу");
+
+            }
+
+        }
+        private void UpdateTaskList()
+        {
+            lstTasks.Items.Clear();
+            foreach (var task in tasks)
+            {
+                lstTasks.Items.Add(task);
+            }
+        }
+
+        private void SaveTasks()
+        {
+            File.WriteAllLines(FilePath, tasks);
+        }
+
+        private void LoadTasks()
+        {
+            if (File.Exists(FilePath))
+            {
+                tasks = File.ReadAllLines(FilePath).ToList();
+                UpdateTaskList();
+            }
+        }
+
+        private void txtTask_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
